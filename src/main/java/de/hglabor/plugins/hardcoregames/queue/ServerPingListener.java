65a576d5b819c03fe9fb6a5c3f8b21adf6158cff
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-07 03:47:04 +0100

    Added Basic Counter for Testing Queue

--- src/main/java/de/hglabor/plugins/hardcoregames/queue/ServerPingListener.java
+++ src/main/java/de/hglabor/plugins/hardcoregames/queue/ServerPingListener.java
@@ -6,16 +6,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ServerPingListener implements Listener {
+
+    private final static AtomicInteger counter = new AtomicInteger(120);
 
     @EventHandler
     public void onPing(PaperServerListPingEvent event) {
         HGInfo hgInfo = new HGInfo(
                 Bukkit.getMaxPlayers(),
                 Bukkit.getOnlinePlayers().size(),
-                60,
+                counter.decrementAndGet(),
                 "LOBBY",
                 Bukkit.getPort());
         event.setMotd(HardcoreGames.GSON.toJson(hgInfo, HGInfo.class));
+        if (counter.get() <= 20) {
+            counter.set(120);
+        }
     }
 }
