bbea647a5df48936d10cf31d23da42765d8e3acc
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-13 09:35:32 +0100

    QueueListener now uses correct values

--- src/main/java/de/hglabor/plugins/hardcoregames/queue/ServerPingListener.java
+++ src/main/java/de/hglabor/plugins/hardcoregames/queue/ServerPingListener.java
@@ -2,6 +2,7 @@ package de.hglabor.plugins.hardcoregames.queue;
 
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import de.hglabor.plugins.hardcoregames.HardcoreGames;
+import de.hglabor.plugins.hardcoregames.game.GameStateManager;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,19 +11,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerPingListener implements Listener {
 
-    private final static AtomicInteger counter = new AtomicInteger(120);
-
     @EventHandler
     public void onPing(PaperServerListPingEvent event) {
         HGInfo hgInfo = new HGInfo(
                 Bukkit.getMaxPlayers(),
-                Bukkit.getOnlinePlayers().size(),
-                counter.decrementAndGet(),
-                "LOBBY",
+                GameStateManager.INSTANCE.getPhase().getCurrentParticipants(),
+                GameStateManager.INSTANCE.getTimer(),
+                GameStateManager.INSTANCE.getPhase().getType().name(),
                 Bukkit.getPort());
         event.setMotd(HardcoreGames.GSON.toJson(hgInfo, HGInfo.class));
-        if (counter.get() <= 20) {
-            counter.set(120);
-        }
     }
 }
